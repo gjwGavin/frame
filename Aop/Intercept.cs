456fc7cf9bd12cddbefc6a2b6e0d8060ce1990f9
Gujiwei <gjw1206545322@163.com>
2021-09-09 15:52:04 +0800

    增加serilog的日志记录

--- Aop/Intercept.cs
+++ Aop/Intercept.cs
@@ -42,23 +42,24 @@ namespace WebApplication1.Aop
 
             if (filterContext.Exception != null)
             {
-                string LogFilePath(string LogEvent) => $@"{AppContext.BaseDirectory}00_Logs\{LogEvent}\log.log";
-                string SerilogOutputTemplate = "{NewLine}{NewLine}Date：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}LogLevel：{Level}{NewLine}Message：{Message}{NewLine}{Exception}" + new string('-', 50);
+               // string LogFilePath(string LogEvent) => $@"{AppContext.BaseDirectory}Logs\{LogEvent}\log.log";
+               // string SerilogOutputTemplate = "{NewLine}{NewLine}Date：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}LogLevel：{Level}{NewLine}Message：{Message}{NewLine}{Exception}" + new string('-', 50);
 
-                Log.Logger = new LoggerConfiguration()
-               .Enrich.FromLogContext()
-               .MinimumLevel.Debug() // 所有Sink的最小记录级别
-               .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Debug).WriteTo.File(LogFilePath("Debug"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
-               .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Information).WriteTo.File(LogFilePath("Information"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
-               .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Warning).WriteTo.File(LogFilePath("Warning"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
-               .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Error).WriteTo.File(LogFilePath("Error"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
-               .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Fatal).WriteTo.File(LogFilePath("Fatal"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
-               .CreateLogger();
+               // Log.Logger = new LoggerConfiguration()
+               //.Enrich.FromLogContext()
+               //.MinimumLevel.Debug() // 所有Sink的最小记录级别
+               //.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Debug).WriteTo.File(LogFilePath("Debug"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+               //.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Information).WriteTo.File(LogFilePath("Information"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+               //.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Warning).WriteTo.File(LogFilePath("Warning"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+               //.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Error).WriteTo.File(LogFilePath("Error"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+               //.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Fatal).WriteTo.File(LogFilePath("Fatal"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+               //.CreateLogger();
                 string controllName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
                 string actionName = filterContext.ActionDescriptor.ActionName;
                // filterContext.HttpContext.Request;
-                Log.Information(string.Format(controllName+ "/"+actionName));
-                Log.Error(string.Format(controllName + "/" + actionName)+ filterContext.Exception.Message);
+                SeriLogTool.Information(string.Format(controllName+ "/"+actionName+"111"));
+                SeriLogTool.Error(string.Format(controllName + "/" + actionName)+ filterContext.Exception.Message+"222");
+
 
             }
 
---
+++ Tool/SeriLogTool.cs
@@ -0,0 +1,66 @@
+﻿using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Tool
+{
+    public class SeriLogTool
+    {
+
+        public static string LogFilePath(string LogEvent) => $@"{AppContext.BaseDirectory}Logs\{LogEvent}\log.log";
+        public static string SerilogOutputTemplate = "{NewLine}{NewLine}日期：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}日志级别：{Level}{NewLine}信息：{Message}{NewLine}{Exception}" + new string('-', 50);
+
+
+        public static void Debug(string message)
+        {
+            Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .MinimumLevel.Debug()
+           .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Information).WriteTo.File(LogFilePath("Debug"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+           .CreateLogger();
+            Log.Debug(message);
+        }
+        public static void Information(string message)
+        {
+            Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .MinimumLevel.Debug()
+           .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Information).WriteTo.File(LogFilePath("Information"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+           .CreateLogger();
+            Log.Information(message);
+        }
+        public static void Warning(string message)
+        {
+            Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .MinimumLevel.Debug()
+           .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Warning).WriteTo.File(LogFilePath("Warning"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+           .CreateLogger();
+            Log.Warning(message);
+        }
+        public static void Error(string message)
+        {
+            Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .MinimumLevel.Debug()
+           .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Error).WriteTo.File(LogFilePath("Error"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+           .CreateLogger();
+            Log.Error(message);
+        }
+        public static void Fatal(string message)
+        {
+            Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .MinimumLevel.Debug()
+           .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Fatal).WriteTo.File(LogFilePath("Fatal"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+           .CreateLogger();
+            Log.Fatal(message);
+        }
+
+
+
+    }
+}
